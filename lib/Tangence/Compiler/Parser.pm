2340bbff90553c7ef17eddd2b680006375685882
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2010-09-06 11:11:54 +0100

    Initial implementation of parser for .tan interface files

---
+++ lib/Tangence/Compiler/Parser.pm
@@ -0,0 +1,316 @@
+package Tangence::Compiler::Parser;
+
+use strict;
+use warnings;
+
+use feature qw( switch ); # we like given/when
+
+use File::Slurp qw( slurp );
+
+use Tangence::Constants;
+
+sub from_file
+{
+   my $class = shift;
+   my ( $filename ) = @_;
+
+   $class->from_string( scalar slurp $filename );
+}
+
+sub from_string
+{
+   my $class = shift;
+   my ( $str ) = @_;
+
+   my $self = bless {
+      str => $str,
+
+      line => 1,
+   }, $class;
+
+   return $self->parse;
+}
+
+sub expect
+{
+   my $self = shift;
+   my ( $expect ) = @_;
+
+   ref $expect or $expect = qr/\Q$expect/;
+
+   $self->skip_ws;
+   $self->{str} =~ m/\G$expect/gc or
+      $self->fail( "Expected $expect" );
+}
+
+sub skip_ws
+{
+   my $self = shift;
+
+   $self->{str} =~ m/\G([\s\n]+)/gc;
+
+   # Since this is the only method that can consume linefeeds, we'll account
+   # the line number here
+   $self->{line} += ( $1 =~ tr/\n// ) if defined $1;
+}
+
+sub parse_ident
+{
+   my $self = shift;
+
+   $self->skip_ws;
+
+   return undef if $self->at_eos;
+
+   $self->{str} =~ m/\G([\w.]+)/gc or
+      $self->fail( "Expected identifier" );
+
+   return $1;
+}
+
+sub parse_kw
+{
+   my $self = shift;
+   my @acceptable = @_;
+
+   $self->skip_ws;
+
+   my $pos = pos $self->{str};
+
+   defined( my $kw = $self->parse_ident ) or
+      return undef;
+
+   grep { $_ eq $kw } @acceptable or
+      pos($self->{str}) = $pos, $self->fail( "Expected any of ".join( ", ", @acceptable ) );
+
+   return $kw;
+}
+
+sub fail
+{
+   my $self = shift;
+   my ( $message ) = @_;
+
+   my $pos = pos $self->{str};
+   my $str = $self->{str};
+
+   my $sol = $pos;
+   $sol-- while $sol > 0 and substr( $str, $sol, 1 ) !~ m/^[\r\n]$/;
+
+   my $eol = $pos;
+   $eol++ while $eol < length($str) and substr( $str, $eol, 1 ) !~ m/^[\r\n]$/;
+
+   my $line = substr( $str, $sol, $eol - $sol );
+   my $col = $pos - $sol;
+
+   die "$message on line $self->{line} at:\n$line\n" . 
+       ( " " x ($col-1) . "^" ) . "\n";
+}
+
+sub enter_scope
+{
+   my $self = shift;
+   my ( $start, $stop, $code ) = @_;
+
+   ref $stop or $stop = qr/\Q$stop/;
+
+   $self->expect( $start );
+   local $self->{endofscope} = $stop;
+
+   $code->();
+
+   $self->expect( $stop );
+}
+
+sub at_eos
+{
+   my $self = shift;
+
+   # DO NOT alter pos() here
+   my $pos = pos $self->{str};
+
+   return 1 if defined $pos and $pos >= length $self->{str};
+
+   return 0 unless defined $self->{endofscope};
+
+   my $at_eos = $self->{str} =~ m/\G$self->{endofscope}/;
+
+   pos($self->{str}) = $pos;
+
+   return $at_eos;
+}
+
+sub maybe
+{
+   my $self = shift;
+   my ( $code ) = @_;
+
+   my $pos = pos $self->{str};
+
+   eval { $code->(); 1 } or pos($self->{str}) = $pos;
+}
+
+sub parse
+{
+   my $self = shift;
+
+   my %package;
+
+   while(1) {
+      given( $self->parse_kw(qw( class )) ) {
+         when( undef ) {
+            # EOF
+            return \%package;
+         }
+         when( 'class' ) {
+            my $classname = $self->parse_ident;
+
+            exists $package{$classname} and
+               $self->fail( "Already have a class called $classname" );
+
+            $self->enter_scope( '{', '}', sub {
+               $package{$classname} = $self->parse_classblock;
+            } );
+         }
+         default {
+            $self->fail( "Expected keyword, found $_" );
+         }
+      }
+   }
+}
+
+sub parse_classblock
+{
+   my $self = shift;
+
+   my %class;
+
+   while(1) {
+      given( $self->parse_kw(qw( method event prop smashed )) ) {
+         when( undef ) {
+            last;
+         }
+
+         when( 'method' ) {
+            my $methodname = $self->parse_ident;
+
+            exists $class{methods}{$methodname} and
+               $self->fail( "Already have a method called $methodname" );
+
+            my $mdef = $class{methods}{$methodname} = {};
+
+            $self->enter_scope( '(', ')', sub {
+               $mdef->{args} = $self->parse_typelist;
+            } );
+
+            $self->expect( '->' );
+
+            $mdef->{ret} = $self->parse_type;
+         }
+
+         when( 'event' ) {
+            my $eventname = $self->parse_ident;
+
+            exists $class{events}{$eventname} and
+               $self->fail( "Already have an event called $eventname" );
+
+            my $edef = $class{events}{$eventname} = {};
+
+            $self->enter_scope( '(', ')', sub {
+               $edef->{args} = $self->parse_typelist;
+            } );
+         }
+
+         my $smashed = 0;
+         when( 'smashed' ) {
+            $smashed = 1;
+
+            $self->expect( 'prop' );
+
+            $_ = 'prop'; continue; # goto case 'prop'
+         }
+         when( 'prop' ) {
+            my $propname = $self->parse_ident;
+
+            exists $class{props}{$propname} and
+               $self->fail( "Already have a property called $propname" );
+
+            my $pdef = $class{props}{$propname} = {};
+
+            $pdef->{smash}++ if $smashed;
+
+            $self->expect( '=' );
+
+            my $dim = DIM_SCALAR;
+            $self->maybe( sub {
+               $dim = $self->parse_dim;
+               $self->expect( 'of' );
+            } );
+
+            $pdef->{type} = $self->parse_type;
+
+            $pdef->{dim} = $dim;
+         }
+      }
+
+      $self->expect( ';' );
+   }
+
+   return \%class;
+}
+
+sub parse_typelist
+{
+   my $self = shift;
+
+   my @ret;
+
+   while(1) {
+      push @ret, $self->parse_type;
+
+      $self->skip_ws;
+      $self->{str} =~ m/\G,/gc or last;
+   }
+
+   return \@ret;
+}
+
+my @basic_types = qw(
+   bool
+   int
+   s8 s16 s32 s64 u8 u16 u32 u64
+   str
+   obj
+   any
+);
+
+sub parse_type
+{
+   my $self = shift;
+
+   my $typename = $self->parse_ident;
+
+   grep { $_ eq $typename } @basic_types or
+      $self->fail( "'$typename' is not a typename" );
+
+   return $typename;
+}
+
+my %dimensions = (
+   scalar => DIM_SCALAR,
+   hash   => DIM_HASH,
+   queue  => DIM_QUEUE,
+   array  => DIM_ARRAY,
+   objset => DIM_OBJSET,
+);
+
+sub parse_dim
+{
+   my $self = shift;
+
+   my $dimname = $self->parse_kw( keys %dimensions );
+
+   return $dimensions{$dimname};
+}
+
+1;
---
+++ t/50compiler-parser.t
@@ -0,0 +1,62 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Test::More tests => 3; 
+
+use Tangence::Compiler::Parser;
+
+use Tangence::Constants;
+
+my $meta;
+
+$meta = Tangence::Compiler::Parser->from_file( "t/Ball.tan" );
+is_deeply( $meta,
+   {
+      't.Ball' => {
+         methods => {
+            bounce => { args => [qw( str )], ret => 'str' },
+         },
+         events => {
+            bounced => { args => [qw( str )] },
+         },
+         props => {
+            colour => { dim => DIM_SCALAR, type => 'str' },
+            size   => { dim => DIM_SCALAR, type => 'int', smash => 1 },
+         },
+      },
+   },
+   'parsed t/Ball.tan'
+);
+
+$meta = Tangence::Compiler::Parser->from_file( "t/Bag.tan" );
+is_deeply( $meta,
+   {
+      't.Bag' => {
+         methods => {
+            add_ball  => { args => [qw( str )], ret => 'obj' },
+            get_ball  => { args => [qw( str )], ret => 'obj' },
+            pull_ball => { args => [qw( str )], ret => 'obj' },
+         },
+         props => {
+            colours => { dim => DIM_HASH, type => 'int' },
+         },
+      },
+   },
+   'parsed t/Bag.tan'
+);
+
+$meta = Tangence::Compiler::Parser->from_file( "t/TestObj.tan" );
+is_deeply( $meta,
+   {
+      't.TestObj' => {
+         props => {
+            array  => { dim => DIM_ARRAY,  type => 'int' },
+            hash   => { dim => DIM_HASH,   type => 'int' },
+            queue  => { dim => DIM_QUEUE,  type => 'int' },
+            scalar => { dim => DIM_SCALAR, type => 'int' },
+         },
+      },
+   },
+   'parsed t/TestObj.tan'
+);
